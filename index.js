6101fda7b4c6e2c8eea8d6bef827b69ae2acf2f5
Ramzy Badawy <Ramzy.Badawy@gmail.com>
2020-09-30 09:13:41 -0400

    Done.

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const testVar = "hi"
 document.querySelector("main#main").remove();
 
 const newHeader = document.createElement("h1");
+newHeader.className = "victory";
 newHeader.id = "victory"
 newHeader.innerHTML = "Flatiron School is the champion!";
-newHeader.className = "victory";
